3837226aebe9e20a3368d573e0ba1dd8a6f757f1
Jason Dude <56562558+JasonDude16@users.noreply.github.com>
2022-01-25 20:55:55 -0500

    attempt to install packages with renv

--- setup/setup.R
+++ setup/setup.R
@@ -1,20 +1,21 @@
 install.packages("reticulate")
 library(reticulate)
 
-# install miniconda, which includes python 
+# install miniconda/python 
 install_miniconda()
 
 # get the reticulate install 
 (cl <- conda_list())
 retic <- cl[cl$name == "r-reticulate", ]$python
 
-if (.Platform$OS.type == "windows") 
+if (.Platform$OS.type == "windows") {
     retic <- gsub('"', "", gsub("\\\\", "/", retic)) 
+}
 
 # add reticulate python version to R Profile (only for local project)
 ll <- readLines(".Rprofile")
 
-if (!exists("done") || !done) {
+if (length(ll) == 1 && !exists("done")) {
     writeLines(text = paste0(ll, "\n", paste0("Sys.setenv(RETICULATE_PYTHON = '", retic, "')")), con = "./.Rprofile")
     done <- TRUE
 }
@@ -22,5 +23,12 @@ if (!exists("done") || !done) {
 # restarting R so .Rprofile can update
 .rs.restartR()
 
+# select yes when prompted
+renv::status()
+renv::hydrate()
+renv::status()
+renv::snapshot()
+
 # very hacky way to do this but should work since versions aren't critical
 py_install(read.table("requirements.txt", header = F, sep = "=")[[1]])
+
